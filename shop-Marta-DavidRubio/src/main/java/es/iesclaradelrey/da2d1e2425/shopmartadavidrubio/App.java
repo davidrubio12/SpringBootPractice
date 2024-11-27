b5bd731d2e7084fa9dd4e9b81d69edcb210d5171
Kisen <158491055+davidrubio12@users.noreply.github.com>
2024-11-27 20:15:15 +0100

    push

--- shop-Marta-DavidRubio/src/main/java/es/iesclaradelrey/da2d1e2425/shopmartadavidrubio/App.java
+++ shop-Marta-DavidRubio/src/main/java/es/iesclaradelrey/da2d1e2425/shopmartadavidrubio/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShopMartaDavidRubioApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShopMartaDavidRubioApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
---
+++ shop-Marta-DavidRubio/src/main/java/es/iesclaradelrey/da2d1e2425/shopmartadavidrubio/controllers/HomeController.java
@@ -0,0 +1,4 @@
+package es.iesclaradelrey.da2d1e2425.shopmartadavidrubio.controllers;
+
+public class HomeController {
+}
