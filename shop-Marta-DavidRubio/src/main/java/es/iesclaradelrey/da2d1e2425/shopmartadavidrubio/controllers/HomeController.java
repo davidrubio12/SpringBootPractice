2e4e78bd89bd52f3754b8d1091aa9b2ad96eabfa
Kisen <158491055+davidrubio12@users.noreply.github.com>
2024-12-02 17:07:15 +0100

    sd

--- shop-Marta-DavidRubio/src/main/java/es/iesclaradelrey/da2d1e2425/shopmartadavidrubio/controllers/HomeController.java
+++ shop-Marta-DavidRubio/src/main/java/es/iesclaradelrey/da2d1e2425/shopmartadavidrubio/controllers/HomeController.java
@@ -1,4 +1,10 @@
 package es.iesclaradelrey.da2d1e2425.shopmartadavidrubio.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping({"", "/"})
 public class HomeController {
+
 }
