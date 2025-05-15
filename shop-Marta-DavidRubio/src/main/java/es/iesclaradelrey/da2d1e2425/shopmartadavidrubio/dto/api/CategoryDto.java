ea9a9e986ce05b3a7b5a592386216e4868afe151
David Rubio <158491055+davidrubio12@users.noreply.github.com>
2025-05-15 20:26:53 +0200

    Update CategoryDto.java

--- shop-Marta-DavidRubio/src/main/java/es/iesclaradelrey/da2d1e2425/shopmartadavidrubio/dto/api/CategoryDto.java
+++ shop-Marta-DavidRubio/src/main/java/es/iesclaradelrey/da2d1e2425/shopmartadavidrubio/dto/api/CategoryDto.java
@@ -1,5 +1,10 @@
 package es.iesclaradelrey.da2d1e2425.shopmartadavidrubio.dto.api;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CategoryDto {
     private Long id;
     private String name;
